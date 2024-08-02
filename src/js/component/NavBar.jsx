c8d42ec88d6b8bc71c5274ff7535c786dffb88b2
Davinia pd <delgadavi@gmail.com>
2024-08-02 20:06:32 +0000

    cambiado contenido del navbar

--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -5,34 +5,32 @@ const NavBar = () => {
         // <h1>Navbar</h1>
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Expand at sm</a>
+                <a className="navbar-brand" href="#">Cartoon Saloon</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarsExample03">
-                    <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+                    <ul className="navbar-nav ms-auto mb-2 mb-sm-0">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <a className="nav-link" href="#">About Us</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
+
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                            <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                                <li><a className="dropdown-item" href="#">Feature Films</a></li>
+                                <li><a className="dropdown-item" href="#">Short Films</a></li>
+                                <li><a className="dropdown-item" href="#">TV Series</a></li>
+                            </ul>    
+                        </li>                    
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
                         </li>
                     </ul>
-                    <form role="search">
-                        <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
-                    </form>
                 </div>
             </div>
         </nav>
