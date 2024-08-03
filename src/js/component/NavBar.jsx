13acc6468845b4a371d2d20288553d515059d8af
Davinia pd <delgadavi@gmail.com>
2024-08-03 14:25:06 +0000

    navbar con logo y verde

--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Logo from "/src/img/logo-cartoon-blanco.png"
+
+const NavBarStyles = {
+    background: "linear-gradient(to right, #169b62,  #ffffff)",
+    fontFamily: 'Montserrat, sans-serif',
+
+}
+
+const logoStyles = {
+    maxWidth: "125px", // Ajusta el ancho máximo del logo según tus necesidades
+    height: "auto", // Permite que la altura se ajuste automáticamente al ancho
+}
 
 const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
+        <nav className="navbar navbar-expand-sm navbar-dark px-5 py-1" style={NavBarStyles} aria-label="Third navbar example">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Cartoon Saloon</a>
+            <img src={Logo} style={logoStyles} alt="" />
+                {/* <a className="navbar-brand fs-3" href="#"><b>Cartoon Saloon</b></a> */}
+
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -12,22 +26,22 @@ const NavBar = () => {
                 <div className="collapse navbar-collapse" id="navbarsExample03">
                     <ul className="navbar-nav ms-auto mb-2 mb-sm-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active text-secondary" aria-current="page" href="#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">About Us</a>
+                            <a className="nav-link text-secondary" href="#">About Us</a>
                         </li>
 
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Feature Films</a></li>
-                                <li><a className="dropdown-item" href="#">Short Films</a></li>
-                                <li><a className="dropdown-item" href="#">TV Series</a></li>
+                            <a className="nav-link dropdown-toggle text-secondary" href="#" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
+                            <ul className="dropdown-menu bg-light bg-opacity-25">
+                                <li><a className="dropdown-item text-secondary" href="#">Feature Films</a></li>
+                                <li><a className="dropdown-item text-secondary" href="#">Short Films</a></li>
+                                <li><a className="dropdown-item text-secondary" href="#">TV Series</a></li>
                             </ul>    
                         </li>                    
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
+                            <a className="nav-link text-secondary" href="#">Contact</a>
                         </li>
                     </ul>
                 </div>
