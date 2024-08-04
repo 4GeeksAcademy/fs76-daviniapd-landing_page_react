407638e4408c39cc54606a3052b28168e68a962c
Davinia pd <delgadavi@gmail.com>
2024-08-04 13:37:04 +0000

    contenido de las cards rellenadas incluyendo el cambio de images por videos

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const iframeRef = React.createRef();
+    
+const handleFullScreen = () => {
+    iframeRef.current.requestFullscreen();
+  };
+
 const Card = (props) => {
+
     return (
         <div className="card col m-3 p-0">
-            <img src={props.imageCard} className="card-img-top" alt="..." />
+            <iframe src={props.videoCard} 
+            frameborder="0" 
+            title="Cartoon Saloon Trailer" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" 
+            allowfullscreen = "true">
+            </iframe>
             <div className="card-body">
                 <h5 className="card-title">{props.titleCard}</h5>
                 <p className="card-text">{props.descriptionCard}</p>
-                <a href={props.urlButton} className="btn btn-primary">{props.labelButton}</a>
+                <div className="d-flex justify-content-center">
+                <a href={props.urlButton} className="btn btn-success">{props.labelButton}</a>
+                </div>
             </div>
         </div>
 
@@ -16,7 +31,7 @@ const Card = (props) => {
 };
 
 Card.propTypes = {
-    imageCard: PropTypes.string,
+    videoCard: PropTypes.string,
     titleCard: PropTypes.string,
     descriptionCard: PropTypes.string,
     urlButton: PropTypes.string,
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -15,32 +15,32 @@ const Home = () => {
 			<Jumbotron />
 			<div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-xl-5 m-3 g-3 d-flex justify-content-around">
 				<Card
-					imageCard="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4dx7_rsQyERNz81p6Yu8JnpS_uLtHma7yRA&s"
-					titleCard="Título de la tarjeta 1"
-					descriptionCard="Descripción de la tarjeta 1"
-					urlButton=""
-					labelButton="Texto del botón 1"
+					videoCard= "https://www.youtube.com/embed/kHhmfNgQ2Bg"
+					titleCard="My Father's Dragon"
+					descriptionCard="2022. Elmer Elevator searches for a captive Dragon on Wild Island and finds much more than he could ever have anticipated."
+					urlButton="https://www.cartoonsaloon.ie/my-fathers-dragon/"
+					labelButton="About this movie"
 				/>
 				<Card
-					imageCard="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4dx7_rsQyERNz81p6Yu8JnpS_uLtHma7yRA&s"
-					titleCard="Título de la tarjeta 2"
-					descriptionCard="Descripción de la tarjeta 2"
-					urlButton=""
-					labelButton="Texto del botón 2"
+					videoCard= "https://www.youtube.com/embed/Zgsfht2YEhc"
+					titleCard="WolfWalkers"
+					descriptionCard="2020. After moving to Ireland, young Robyn meets Mebh, a free-spirited girl from a unusual tribe who is said to transform into a wolf at night."
+					urlButton="https://www.cartoonsaloon.ie/wolfwalkers/"
+					labelButton="About this movie"
 				/>
 				<Card
-					imageCard="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4dx7_rsQyERNz81p6Yu8JnpS_uLtHma7yRA&s"
-					titleCard="Título de la tarjeta 3"
-					descriptionCard="Descripción de la tarjeta 3"
-					urlButton=""
-					labelButton="Texto del botón 3"
+					videoCard= "https://www.youtube.com/embed/CCvR5YBcpR8" 
+					titleCard="Song of the Sea"
+					descriptionCard="2014. Ben, a young Irish boy, and his little sister Saoirse, a girl who can turn into a seal, go on an adventure to free the fairies and save the spirit world. "
+					urlButton="https://www.cartoonsaloon.ie/song-of-the-sea/"
+					labelButton="About this movie"
 				/>
 				<Card
-					imageCard="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4dx7_rsQyERNz81p6Yu8JnpS_uLtHma7yRA&s"
-					titleCard="Título de la tarjeta 4"
-					descriptionCard="Descripción de la tarjeta 4"
-					urlButton=""
-					labelButton = "Texto del botón 4"
+					videoCard= "https://www.youtube.com/embed/lw2_HZTuQBE"
+					titleCard="The Book of Kells"
+					descriptionCard="2009. A young boy in a remote medieval meets a celebrated master illuminator arrives with an ancient book, brimming with secret wisdom and powers."
+					urlButton="https://www.cartoonsaloon.ie/the-secret-of-kells/"
+					labelButton="About this movie"
 				/>
 			</div>
 			<Footer />
